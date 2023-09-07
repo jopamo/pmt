8557cf61407af10e16b3311b335e3c62d1812cd6
jopamo <p@1g4.org>
2023-09-06 21:59:54 -0400

    improve spacing

--- hw1.cpp
+++ hw1.cpp
@@ -10,19 +10,20 @@ bool intCheck(string input) {
     if (!isdigit(input[i]))
       return false;
   }
+
   return true;
 }
 
 int str2int(string input) {
   bool isInt = intCheck(input);
 
-  if(isInt) {
+  if (isInt) {
     //don't give stoi() anything big, 8 char wide is beyond spec
     input.resize(8);
     return stoi(input);
   }
-  else
-    return -1;
+
+  return -1;
 }
 
 int roll(int min, int max) {
@@ -40,7 +41,7 @@ int dimCheck(string str) {
     cout << "Dimension has to be larger than 1. Please write a number between 1 to 99." << endl;
     return -1;
   }
-  else if(dimension > 99) {
+  else if (dimension > 99) {
     cout << "Dimension has to smaller than 100. Please write a number between 1 to 99." << endl;
     return -1;
   }
@@ -57,7 +58,7 @@ int moveCheck(string str) {
     cout << "Dimension has to be larger than 1. Please write a number between 1 to 1000000." << endl;
     return -1;
   }
-  else if(maxMoves > 1000000) {
+  else if (maxMoves > 1000000) {
     cout << "Dimension has to smaller than 1000000. Please write a number between 1 to 1000000." << endl;
     return -1;
   }
@@ -71,14 +72,14 @@ int main() {
   int maxMoves = -1;
 
   /* Ask the user to enter a dimension*/
-  while(dimension == -1) {
+  while (dimension == -1) {
     cout << "Enter a number between 1 to 99 for dimension: ";
     cin >> userString;
     dimension = dimCheck(userString);
   }
 
   /* Ask the user to enter a maxMoves*/
-  while(maxMoves == -1) {
+  while (maxMoves == -1) {
     cout << "Enter a number between 1 to 1000000 for maximum movements: ";
     cin >> userString;
     maxMoves = moveCheck(userString);
